e8d7537b73b590bf92ddf7a373d232e9d339cb14
Wendi Cui <wcui@igrace.com>
2017-11-22 12:29:07 -0500

    module added

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+var orm = require('../config/orm.js')
+
+var burger = {
+
+	all: function(cb){
+		orm.all("burgers", cb)
+	}
+
+	select: function(condition, cb){
+		orm.select("burgers", condition, cb)
+	}
+
+	update:function(condition, value, cb){
+		orm.update("burgers", condition, value, cb)
+	}
+
+	create: function(columns, value, cb){
+		orm.create("burgers", columns, value, cb)
+	}
+
+}
+
+module.exports = burger
