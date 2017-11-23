02c00a5f3e05b04a3aa04a65535147ee95b110d4
Yaya Du <Yayadu@Yayas-MBP.fios-router.home>
2017-11-23 13:49:51 -0500

    server set up

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var bodyParser = require("body-parser")
+
+//set port
+var port = 3000 || process.env.PORT
+
+//create server
+var app = express();
+
+//use body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//create route for public folder file
+app.use(express.static("public"));
+
+//Set Handlebars
+var handleBar = require("express-handlebars");
+
+app.engine("handlebars", handleBar({defaultLayout:"main"}));
+app.set("view engine", "handlebars");
+
+//import routes
+var routes = require("./controllers/burgers_controllers.js");
+
+app.use("/", routes);
+
+app.listen(port);
